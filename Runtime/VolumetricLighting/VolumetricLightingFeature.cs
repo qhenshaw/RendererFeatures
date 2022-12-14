2eb332c19b41b72561efc2eaf2c7782e1909c98f
Quin Henshaw <qhenshaw@gmail.com>
2022-12-13 23:19:58 -0800

    completely reworked volumetric lights, now supports all visible lights and realtime shadows

--- Runtime/VolumetricLighting/VolumetricLightingFeature.cs
+++ Runtime/VolumetricLighting/VolumetricLightingFeature.cs
@@ -1,150 +1,101 @@
-namespace UnityEngine.Rendering.Universal
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class VolumetricLightingFeature : ScriptableRendererFeature
 {
-    public class VolumetricLightingFeature : ScriptableRendererFeature
+    class Pass : ScriptableRenderPass
     {
-        /// <summary>
-        /// Draws full screen mesh using given material and pass and reading from source target.
-        /// </summary>
-        private class VolumetricLightingPass : ScriptableRenderPass
+        public Settings Settings;
+
+
+        RenderTargetIdentifier source;
+        RenderTargetIdentifier tmpRT0;
+        RenderTargetIdentifier downSampleRT;
+
+        FilteringSettings filteringSettings;
+        RenderStateBlock renderStateBlock;
+        readonly List<ShaderTagId> _shaderTagIds = new List<ShaderTagId>();
+
+        int tmpRT0ID = Shader.PropertyToID("tmpRT0ID");
+        int downSampleRTID = Shader.PropertyToID("downSampleRT");
+
+        public Pass(Settings settings)
         {
-            public Settings settings;
+            Settings = settings;
 
-            RenderTargetIdentifier source;
-            RenderTargetIdentifier volumetric;
-            RenderTargetIdentifier temp0;
-            RenderTargetIdentifier temp1;
-
-            int sourceID = Shader.PropertyToID("_Source");
-            int volumetricID = Shader.PropertyToID("_Volumetric");
-            int temp0ID = Shader.PropertyToID("_Temp0");
-            int temp1ID = Shader.PropertyToID("_Temp1");
-
-            Vector4[] lightPositions;
-            float[] lightRanges;
-            Vector4[] lightColors;
-
-            public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
-            {
-                source = renderingData.cameraData.renderer.cameraColorTarget;
-            }
-
-            public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
-            {
-                cameraTextureDescriptor.depthBufferBits = 0;
-
-                var width = cameraTextureDescriptor.width / settings.downSample;
-                var height = cameraTextureDescriptor.height / settings.downSample;
-
-                cmd.GetTemporaryRT(volumetricID, cameraTextureDescriptor);
-                cmd.GetTemporaryRT(temp0ID, width, height, 0, FilterMode.Bilinear, RenderTextureFormat.ARGB32);
-                cmd.GetTemporaryRT(temp1ID, width, height, 0, FilterMode.Bilinear, RenderTextureFormat.ARGB32);
-                volumetric = new RenderTargetIdentifier(volumetricID);
-                temp0 = new RenderTargetIdentifier(temp0ID);
-                temp1 = new RenderTargetIdentifier(temp1ID);
-                ConfigureTarget(volumetric);
-                ConfigureTarget(temp0);
-                ConfigureTarget(temp1);
-
-
-                lightPositions = new Vector4[UniversalRenderPipeline.maxVisibleAdditionalLights];
-                lightRanges = new float[UniversalRenderPipeline.maxVisibleAdditionalLights];
-                lightColors = new Vector4[UniversalRenderPipeline.maxVisibleAdditionalLights];
-            }
-
-            /// <inheritdoc/>
-            public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
-            {
-                CommandBuffer cmd = CommandBufferPool.Get("Volumetric Lighting Feature");
-
-                var lightData = renderingData.lightData;
-                var visibleLights = renderingData.lightData.visibleLights;
-                int maxLights = Mathf.Min(visibleLights.Length, UniversalRenderPipeline.maxVisibleAdditionalLights);
-                int lightIndex = 0;
-                for (int i = 0; i < maxLights; i++)
-                {
-                    if (lightData.mainLightIndex == i) continue;
-                    {
-                        VisibleLight light = visibleLights[i];
-                        lightPositions[lightIndex] = light.light.transform.position;
-                        lightRanges[lightIndex] = light.range;
-                        Color color = light.finalColor;
-                        lightColors[lightIndex] = new Vector4(color.r, color.g, color.b, 1f);
-                        lightIndex++;
-                    }
-                }
-
-                cmd.SetGlobalInteger("_PixelLightCount", lightIndex);
-                cmd.SetGlobalVectorArray("_PixelLightPositions", lightPositions);
-                cmd.SetGlobalFloatArray("_PixelLightRanges", lightRanges);
-                cmd.SetGlobalVectorArray("_PixelLightColors", lightColors);
-
-                // volumetric pass
-                cmd.Blit(source, temp1, settings.volumetricMaterial, 0);
-
-                // blur pass
-                cmd.SetGlobalFloat("_offset", 0.5f);
-                cmd.Blit(temp1, temp0, settings.blurMaterial);
-
-                for (int i = 1; i < settings.blurPasses - 1; i++)
-                {
-                    cmd.SetGlobalFloat("_offset", 0.5f + i);
-                    cmd.Blit(temp0, temp1, settings.blurMaterial);
-
-                    var temp = temp0;
-                    temp0 = temp1;
-                    temp1 = temp;
-                }
-
-                cmd.Blit(temp0, temp1, settings.blurMaterial);
-                cmd.SetGlobalTexture("_VolumetricLightingContribution", temp1);
-                //cmd.Blit(temp1, source);
-
-                // composite pass
-                cmd.Blit(source, volumetric, settings.compositeMaterial, 0);
-                cmd.Blit(volumetric, source);
-
-                context.ExecuteCommandBuffer(cmd);
-                CommandBufferPool.Release(cmd);
-            }
-
-            /// <inheritdoc/>
-            public override void FrameCleanup(CommandBuffer cmd)
-            {
-                cmd.ReleaseTemporaryRT(sourceID);
-                cmd.ReleaseTemporaryRT(volumetricID);
-                cmd.ReleaseTemporaryRT(temp0ID);
-                cmd.ReleaseTemporaryRT(temp1ID);
-            }
+            filteringSettings = new FilteringSettings(null, settings.layerMask);
+            renderStateBlock = new RenderStateBlock(RenderStateMask.Nothing);
+            _shaderTagIds.Add(new ShaderTagId("SRPDefaultUnlit"));
         }
 
-        [System.Serializable]
-        public class Settings
+        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
         {
-            public RenderPassEvent renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
-            public int downSample = 2;
-            public Material volumetricMaterial = null;
-            public Material compositeMaterial = null;
-            [HideInInspector] public Material blurMaterial = null;
-            public int blurPasses = 4;
+            RenderTextureDescriptor blitTargetDescriptor = renderingData.cameraData.cameraTargetDescriptor;
+
+            int downSample = 4;
+            int width = blitTargetDescriptor.width / downSample;
+            int height = blitTargetDescriptor.height / downSample;
+
+            source = renderingData.cameraData.renderer.cameraColorTarget;
+            cmd.GetTemporaryRT(tmpRT0ID, blitTargetDescriptor, FilterMode.Bilinear);
+            cmd.GetTemporaryRT(downSampleRTID, width, height, 0, FilterMode.Bilinear, RenderTextureFormat.DefaultHDR);
+
+            tmpRT0 = new RenderTargetIdentifier(tmpRT0ID);
+            downSampleRT = new RenderTargetIdentifier(downSampleRTID);
+
+            ConfigureTarget(tmpRT0);
+            ConfigureTarget(downSampleRT);
+
+            ConfigureClear(ClearFlag.Color, Color.clear);
         }
 
-        public Settings settings = new Settings();
-        VolumetricLightingPass blitPass;
-
-        public override void Create()
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
-            blitPass = new VolumetricLightingPass();
+            CommandBuffer cmd = CommandBufferPool.Get("Volumetric Lighting Feature");
+
+            SortingCriteria sortingCriteria = SortingCriteria.CommonOpaque;
+            DrawingSettings drawingSettings = CreateDrawingSettings(_shaderTagIds, ref renderingData, sortingCriteria);
+
+            context.DrawRenderers(renderingData.cullResults, ref drawingSettings, ref filteringSettings, ref renderStateBlock);
+
+            cmd.SetGlobalTexture("_VolumetricLightingContribution", downSampleRT);
+            cmd.Blit(source, tmpRT0, Settings.compositeMaterial, 0);
+            cmd.Blit(tmpRT0, source);
+
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Clear();
+            CommandBufferPool.Release(cmd);
         }
 
-        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        public override void OnCameraCleanup(CommandBuffer cmd)
         {
-            if (settings.volumetricMaterial == null) return;
-            if (settings.compositeMaterial == null) return;
-            if (settings.blurMaterial == null) settings.blurMaterial = CoreUtils.CreateEngineMaterial(Shader.Find("Hidden/KawaseBlur"));
-
-            blitPass.renderPassEvent = settings.renderPassEvent;
-            blitPass.settings = settings;
-            renderer.EnqueuePass(blitPass);
+            cmd.ReleaseTemporaryRT(tmpRT0ID);
+            cmd.ReleaseTemporaryRT(downSampleRTID);
         }
+    }
+
+    [System.Serializable]
+    public class Settings
+    {
+        public LayerMask layerMask;
+        public Material compositeMaterial;
+    }
+
+    public Settings settings = new Settings();
+    Pass pass;
+
+    public override void Create()
+    {
+        pass = new Pass(settings);
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        if (settings.compositeMaterial == null) return;
+
+        pass.renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+        renderer.EnqueuePass(pass);
     }
 }
--- Runtime/VolumetricLighting/VolumetricLightingIncludes.hlsl
+++ Runtime/VolumetricLighting/VolumetricLightingIncludes.hlsl
@@ -1,175 +1,58 @@
-
-// #pragma multi_compile _MAIN_LIGHT_SHADOWS_CASCADE
-// #pragma multi_compile _SHADOWS_SOFT
-
-void GetMainLight_float( float3 worldoPos, out float3 Direction , out float3 Color, out float ShadowAtten){
-    #ifdef SHADERGRAPH_PREVIEW
-        Direction = float3(1,1,1);
-        Color = float3(1,1,1);
-        ShadowAtten = 1.0f;
-    #else
-    
-    //shadow Coord 만들기 
-    #if defined(_MAIN_LIGHT_SHADOWS_SCREEN)
-    half4 clipPos = TransformWorldToHClip(worldPos);
-    half4 shadowCoord =  ComputeScreenPos(clipPos);
-    #else
-    half4 shadowCoord =  TransformWorldToShadowCoord(worldoPos);
-    #endif
-     
-    Light light = GetMainLight();
-    Direction = light.direction;
-    Color = light.color;
-
-    //메인라이트가 없거나 리시브 셰도우 오프가 되어 있을때 
-    #if !defined(_MAIN_LIGHT_SHADOWS)
-        ShadowAtten = 1.0f;
-    #endif
-
-    //ShadowAtten 받아와서 만들기 
-    #if SHADOWS_SCREEN
-        ShadowAtten = SampleScreenSpaceShadowmap(shadowCoord);
-    #else
-        ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
-        half shadowStrength = GetMainLightShadowStrength();
-        ShadowAtten = SampleShadowmap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowmapTexture, sampler_MainLightShadowmapTexture), shadowSamplingData, shadowStrength, false);
-    #endif
-
-    #endif
+real random01(real2 p)
+{
+    return frac(sin(dot(p, real2(41, 289))) * 45758.5453);
 }
 
-void GetAdditionalLightAttenuation(int index, float3 worldPosition, out float attenuation, out float3 color)
+void AdditionalLights_float(float3 WorldPosition, float2 uv, out float3 color)
 {
-    attenuation = 0;
-        
-    Light light = GetAdditionalLight(index, worldPosition);
-    ShadowSamplingData shadowSamplingData = GetAdditionalLightShadowSamplingData();
-    half4 shadowParams = GetAdditionalLightShadowParams(index);
+    color = 0;
     
-    int shadowSliceIndex = shadowParams.w;
- 
-    UNITY_BRANCH
-    if (shadowSliceIndex < 0)
-    {
-        attenuation = 1.0;
-    }
- 
-    half isPointLight = shadowParams.z;
- 
-    UNITY_BRANCH
-    if (isPointLight)
-    {
-        // This is a point light, we have to find out which shadow slice to sample from
-        float cubemapFaceId = CubeMapFaceID(-light.direction);
-        shadowSliceIndex += cubemapFaceId;
-    }
+#ifndef SHADERGRAPH_PREVIEW
     
-#if USE_STRUCTURED_BUFFER_FOR_LIGHT_DATA
-    float4 shadowCoord = mul(_AdditionalLightsWorldToShadow_SSBO[shadowSliceIndex], float4(worldPosition, 1.0));
-#else
-    float4 shadowCoord = mul(_AdditionalLightsWorldToShadow[shadowSliceIndex], float4(worldPosition, 1.0));
-#endif
- 
-    float shadow = SampleShadowmap(TEXTURE2D_ARGS(_AdditionalLightsShadowmapTexture, sampler_AdditionalLightsShadowmapTexture), shadowCoord, shadowSamplingData, shadowParams, true);
-    attenuation = shadow * light.distanceAttenuation;
-    color = light.color;
-}
-
-float random(float2 uv)
-{
-    return frac(sin(dot(uv, float2(12.9898, 78.233))) * 43758.5453123);
-}
-
-float4 _PixelLightPositions[16];
-float _PixelLightRanges[16];
-float4 _PixelLightColors[16];
-
-void GetAdditionalLightsVolumetric_float(float3 worldPosition, float2 uv, float count, float depth, out float3 lightColor, out float total)
-{
-#ifdef SHADERGRAPH_PREVIEW
-    attenuation = 1;
-#endif
+    float3 worldPosition = WorldPosition;
     
-    total = 0;
-    
-#if !defined(_ADDITIONAL_LIGHT_SHADOWS)
-    total = 1;
-#endif
-    
-    int lightCount = (uint) count;
     float3 camPos = _WorldSpaceCameraPos;
-    float3 rayDirection = normalize(worldPosition - camPos);
-    float depthMax = 15;
-    float density = 0.005;
-    int stepCount = 64;
-    float depthOffset = -0.1;
-    float fragmentDistance = min(depthMax, depth + depthOffset);
-    float stepLength = fragmentDistance / stepCount;
-    float noise = random(uv);
+    float3 rayVec = worldPosition - camPos;
+    float3 rayDirection = normalize(rayVec);
+    float rayLength = length(rayVec);
+    float density = 0.03;
+    int stepCount = 128;
+    float maxDistance = 50;
+    float stepLength = 0.1;
+    float depth = length(worldPosition - camPos);
+    float clampedDepth = min(depth, maxDistance);
+    float scaledStep = clampedDepth / stepCount;
     
-    for (int i = 0; i < stepCount; i++)
+    int pixelLightCount = GetAdditionalLightsCount();
+    uv += float2(1, 1) * _Time.y;
+    float random = random01(uv);
+    float offset = random * 0.1;
+    
+    float startOffset = random * stepLength * 1;
+                
+    for (int j = 0; j < pixelLightCount; j++)
     {
-        for (int j = 0; j < lightCount; j++)
+        for (int i = 0; i < stepCount; i++)
         {
-            float3 samplePosition = camPos + rayDirection * stepLength * i * (1 + noise * 0.1);
-            float attenuation;
-            float3 color;
-            GetAdditionalLightAttenuation(j, samplePosition, attenuation, color);
-            lightColor += color / stepCount;
-            total += attenuation * density;
+            // sample light attenuation
+            float3 samplePosition = camPos + rayDirection * (stepLength * i + startOffset);
+            Light light = GetAdditionalLight(j, samplePosition, half4(1, 1, 1, 1));
+    
+            // depth occlusion
+            float sampleDistance = length(samplePosition - camPos);
+            float penetration = sampleDistance - depth;
+            float depthCull = penetration < 0;
+            
+            // noise
+            float3 noisePosition = samplePosition / _NoiseScale + _Time.y * -_NoiseSpeed;
+            float noise = SAMPLE_TEXTURE3D(_Noise, sampler_Noise, noisePosition).r;
+            noise = lerp(_NoiseRemap.x, _NoiseRemap.y, noise);
+            noise = saturate(noise);
+                
+            // final contribution
+            float lightAttenuation = light.distanceAttenuation * light.shadowAttenuation;
+            color += density * light.color * noise * lightAttenuation * depthCull;
         }
     }
-    
-    //for (int i = 0; i < stepCount; i++)
-    //{
-    //    for (int j = 0; j < lightCount; j++)
-    //    {
-    //        float3 samplePosition = camPos + rayDirection * stepLength * i;
-    //        float attenuation;
-    //        float3 color;
-    //        //GetAdditionalLightAttenuation(j, samplePosition, attenuation, color);
-    //        float3 position = _PixelLightPositions[j].rgb;
-    //        float range = _PixelLightRanges[j];
-    //        float distance = length(samplePosition - position);
-    //        attenuation = pow(1 - saturate(distance / range), 1);
-    //        color = _PixelLightColors[j].rgb;
-    //        lightColor += color / stepCount;
-    //        total += attenuation * density;
-    //    }
-    //}
-    
-    total = saturate(total);
-}
-
-void GetAdditionalLightVolumetric_float(float3 worldPosition, float lightIndex, float depth, out float3 lightColor, out float total)
-{
-#ifdef SHADERGRAPH_PREVIEW
-    attenuation = 1;
 #endif
-    
-    total = 0;
-    
-#if !defined(_ADDITIONAL_LIGHT_SHADOWS)
-    total = 1;
-#endif
-    
-    float3 camPos = _WorldSpaceCameraPos;
-    float3 rayDirection = normalize(worldPosition - camPos);
-    float depthMax = 15;
-    float density = 0.005;
-    int stepCount = 64;
-    float fragmentDistance = min(depthMax, depth);
-    float stepLength = fragmentDistance / stepCount;
-    
-    for (int i = 0; i < stepCount; i++)
-    {
-        float3 samplePosition = camPos + rayDirection * stepLength * i;
-        float attenuation;
-        float3 color;
-        GetAdditionalLightAttenuation(lightIndex, samplePosition, attenuation, color);
-        lightColor += color / stepCount;
-        total += attenuation * density;
-    }
-    
-    total = saturate(total);
 }
