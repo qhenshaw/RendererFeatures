30b6404afe4399565878eff525a3775764f726b7
Quin Henshaw <qhenshaw@gmail.com>
2023-02-12 17:40:26 -0800

    updated using RTHandle, throwing errors and warnings

--- Runtime/DepthFog/DepthFogFeature.cs
+++ Runtime/DepthFog/DepthFogFeature.cs
@@ -10,11 +10,7 @@ namespace UnityEngine.Rendering.Universal
         {
             public DepthFogSettings settings;
 
-            string profilerTag;
-
-            int tmpRT0ID = Shader.PropertyToID("tmpBlurRT0");
-            int tmpRT1ID = Shader.PropertyToID("tmpBlurRT1");
-            int tmpRT2ID = Shader.PropertyToID("tmpBlurRT2");
+            string profilerTag = "Depth Fog";
 
             int colorID = Shader.PropertyToID("_Color");
 
@@ -28,107 +24,103 @@ namespace UnityEngine.Rendering.Universal
             int heightEndID = Shader.PropertyToID("_HeightEnd");
             int heightFalloffID = Shader.PropertyToID("_HeightFalloff");
 
-            RenderTargetIdentifier tmpRT0;
-            RenderTargetIdentifier tmpRT1;
-            RenderTargetIdentifier tmpRT2;
-            public RenderTargetIdentifier Source { get; set; }
-
-            public DepthFogPass()
-            {
-                profilerTag = "DepthFog";
-            }
+            RTHandle tempRT0;
+            RTHandle tempRT1;
+            RTHandle tempRT2;
+            RTHandle sourceRT;
 
             public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
             {
-                var renderer = renderingData.cameraData.renderer;
-                Source = renderer.cameraColorTarget;
+                base.OnCameraSetup(cmd, ref renderingData);
 
-                RenderTextureDescriptor blitTargetDescriptor = renderingData.cameraData.cameraTargetDescriptor;
-                blitTargetDescriptor.depthBufferBits = 0;
-                var width = blitTargetDescriptor.width / settings.downsample;
-                var height = blitTargetDescriptor.height / settings.downsample;
-
-                cmd.GetTemporaryRT(tmpRT0ID, blitTargetDescriptor, FilterMode.Bilinear);
-                cmd.GetTemporaryRT(tmpRT1ID, width, height, 0, FilterMode.Bilinear, RenderTextureFormat.DefaultHDR);
-                cmd.GetTemporaryRT(tmpRT2ID, width, height, 0, FilterMode.Bilinear, RenderTextureFormat.DefaultHDR);
-
-                tmpRT0 = new RenderTargetIdentifier(tmpRT0ID);
-                tmpRT1 = new RenderTargetIdentifier(tmpRT1ID);
-                tmpRT2 = new RenderTargetIdentifier(tmpRT2ID);
+                sourceRT = renderingData.cameraData.renderer.cameraColorTargetHandle;
+                tempRT0 = RTHandles.Alloc(new RenderTargetIdentifier("_TempRT0"), name: "_TempRT0");
+                tempRT1 = RTHandles.Alloc(new RenderTargetIdentifier("_TempRT1"), name: "_TempRT1");
+                tempRT2 = RTHandles.Alloc(new RenderTargetIdentifier("_TempRT2"), name: "_TempRT2");
             }
 
             public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
             {
-                CommandBuffer cmd = CommandBufferPool.Get(profilerTag);
-                cmd.Clear();
-
                 var stack = VolumeManager.instance.stack;
                 var component = stack.GetComponent<DepthFogComponent>();
-                if(component.IsActive())
+                if (!component.IsActive()) return;
+
+                CommandBuffer cmd = CommandBufferPool.Get(profilerTag);
+
+                RenderTextureDescriptor targetDescriptor = renderingData.cameraData.cameraTargetDescriptor;
+                targetDescriptor.depthBufferBits = 0;
+
+                int width = targetDescriptor.width / settings.downsample;
+                int height = targetDescriptor.height / settings.downsample;
+
+                cmd.GetTemporaryRT(Shader.PropertyToID(tempRT0.name), targetDescriptor, FilterMode.Bilinear);
+                cmd.GetTemporaryRT(Shader.PropertyToID(tempRT1.name), width, height, 0, FilterMode.Bilinear, RenderTextureFormat.DefaultHDR);
+                cmd.GetTemporaryRT(Shader.PropertyToID(tempRT2.name), width, height, 0, FilterMode.Bilinear, RenderTextureFormat.DefaultHDR);
+
+                settings.fogMaterial.SetColor(colorID, component.Color.value);
+
+                settings.fogMaterial.SetFloat(depthDensityID, component.DepthDensity.value);
+                settings.fogMaterial.SetFloat(depthStartID, component.DepthStart.value);
+                settings.fogMaterial.SetFloat(depthEndID, component.DepthEnd.value);
+                settings.fogMaterial.SetFloat(depthFalloffID, component.DepthFalloff.value);
+
+                settings.fogMaterial.SetFloat(heightDensityID, component.HeightDensity.value);
+                settings.fogMaterial.SetFloat(heightStartID, component.HeightStart.value);
+                settings.fogMaterial.SetFloat(heightEndID, component.HeightEnd.value);
+                settings.fogMaterial.SetFloat(heightFalloffID, component.HeightFalloff.value);
+
+                settings.compositeMaterial.SetFloat(depthDensityID, component.DepthDensity.value);
+                settings.compositeMaterial.SetFloat(depthStartID, component.DepthStart.value);
+                settings.compositeMaterial.SetFloat(depthEndID, component.DepthEnd.value);
+                settings.compositeMaterial.SetFloat(depthFalloffID, component.DepthFalloff.value);
+
+                settings.compositeMaterial.SetFloat(heightDensityID, component.HeightDensity.value);
+                settings.compositeMaterial.SetFloat(heightStartID, component.HeightStart.value);
+                settings.compositeMaterial.SetFloat(heightEndID, component.HeightEnd.value);
+                settings.compositeMaterial.SetFloat(heightFalloffID, component.HeightFalloff.value);
+
+                // fog pass
+                cmd.Blit(sourceRT, tempRT0, settings.fogMaterial, 0);
+                cmd.Blit(tempRT0, sourceRT);
+
+                if (settings.DepthBlur)
                 {
-                    settings.fogMaterial.SetColor(colorID, component.Color.value);
+                    // first pass
+                    cmd.SetGlobalFloat("_offset", 1.5f);
+                    cmd.Blit(tempRT0, tempRT1, settings.blurMaterial);
 
-                    settings.fogMaterial.SetFloat(depthDensityID, component.DepthDensity.value);
-                    settings.fogMaterial.SetFloat(depthStartID, component.DepthStart.value);
-                    settings.fogMaterial.SetFloat(depthEndID, component.DepthEnd.value);
-                    settings.fogMaterial.SetFloat(depthFalloffID, component.DepthFalloff.value);
-
-                    settings.fogMaterial.SetFloat(heightDensityID, component.HeightDensity.value);
-                    settings.fogMaterial.SetFloat(heightStartID, component.HeightStart.value);
-                    settings.fogMaterial.SetFloat(heightEndID, component.HeightEnd.value);
-                    settings.fogMaterial.SetFloat(heightFalloffID, component.HeightFalloff.value);
-
-                    settings.compositeMaterial.SetFloat(depthDensityID, component.DepthDensity.value);
-                    settings.compositeMaterial.SetFloat(depthStartID, component.DepthStart.value);
-                    settings.compositeMaterial.SetFloat(depthEndID, component.DepthEnd.value);
-                    settings.compositeMaterial.SetFloat(depthFalloffID, component.DepthFalloff.value);
-
-                    settings.compositeMaterial.SetFloat(heightDensityID, component.HeightDensity.value);
-                    settings.compositeMaterial.SetFloat(heightStartID, component.HeightStart.value);
-                    settings.compositeMaterial.SetFloat(heightEndID, component.HeightEnd.value);
-                    settings.compositeMaterial.SetFloat(heightFalloffID, component.HeightFalloff.value);
-
-                    // fog pass
-                    cmd.Blit(Source, tmpRT0, settings.fogMaterial, 0);
-                    cmd.Blit(tmpRT0, Source);
-
-                    if (settings.DepthBlur)
+                    for (var i = 1; i < settings.blurPasses - 1; i++)
                     {
-                        // first pass
-                        cmd.SetGlobalFloat("_offset", 1.5f);
-                        cmd.Blit(tmpRT0, tmpRT1, settings.blurMaterial);
+                        cmd.SetGlobalFloat("_offset", 0.5f + i);
+                        cmd.Blit(tempRT1, tempRT2, settings.blurMaterial);
 
-                        for (var i = 1; i < settings.blurPasses - 1; i++)
-                        {
-                            cmd.SetGlobalFloat("_offset", 0.5f + i);
-                            cmd.Blit(tmpRT1, tmpRT2, settings.blurMaterial);
-
-                            // pingpong
-                            var rttmp = tmpRT1;
-                            tmpRT1 = tmpRT2;
-                            tmpRT2 = rttmp;
-                        }
-
-                        // final pass
-                        cmd.SetGlobalFloat("_offset", 0.5f + settings.blurPasses - 1f);
-                        cmd.Blit(tmpRT1, tmpRT2, settings.blurMaterial);
-                        cmd.SetGlobalTexture("_DepthFogBlurTexture", tmpRT2);
-
-                        // compostite pass
-                        cmd.Blit(Source, tmpRT0, settings.compositeMaterial, 0);
-                        cmd.Blit(tmpRT0, Source);
+                        // pingpong
+                        var rttmp = tempRT1;
+                        tempRT1 = tempRT2;
+                        tempRT2 = rttmp;
                     }
+
+                    // final pass
+                    cmd.SetGlobalFloat("_offset", 0.5f + settings.blurPasses - 1f);
+                    cmd.Blit(tempRT1, tempRT2, settings.blurMaterial);
+                    cmd.SetGlobalTexture("_DepthFogBlurTexture", tempRT2);
+
+                    // compostite pass
+                    cmd.Blit(sourceRT, tempRT0, settings.compositeMaterial, 0);
+                    cmd.Blit(tempRT0, sourceRT);
                 }
 
                 context.ExecuteCommandBuffer(cmd);
                 CommandBufferPool.Release(cmd);
             }
 
-            public override void FrameCleanup(CommandBuffer cmd)
+            public override void OnCameraCleanup(CommandBuffer cmd)
             {
-                cmd.ReleaseTemporaryRT(tmpRT0ID);
-                cmd.ReleaseTemporaryRT(tmpRT1ID);
-                cmd.ReleaseTemporaryRT(tmpRT2ID);
+                base.OnCameraCleanup(cmd);
+
+                tempRT0.Release();
+                tempRT1.Release();
+                tempRT2.Release();
             }
         }
 
